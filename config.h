469b7a6c885fe722748b127d706841df502fdb5a
Michael A. Ferero <mikfer@localhost.localdomain>
2018-09-21 15:31:47 -0400

    Changed font BACK to ibm 3270 - more readable!

--- config.h
+++ config.h
@@ -5,7 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
+/*
 static char *font = "IBM Plex Mono:pixelsize=14:antialias=true:autohint=true";
+*/
+static char *font = "IBM 3270:size=12:antialias=true:autohint=true";
 static int borderpx = 2;
  
 /*
